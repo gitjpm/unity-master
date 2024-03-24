b6c872f4e49f88b89fc35d9bb5ae54f925d4e58d
gitjpm <jpuchades@outlook.com>
2024-03-24 17:56:34 +0100

    changing colors

--- Assets/Scripts/EscalarAlturaCubo.cs
+++ Assets/Scripts/EscalarAlturaCubo.cs
@@ -24,7 +24,7 @@ public class EscalarAlturaCubo : MonoBehaviour
             float escalaYObjetivo = Random.Range(escalaMinima, escalaMaxima);
             escalaObjetivo = new Vector3(1, escalaYObjetivo, 1);
             yield return CambiarEscala(escalaInicial, escalaObjetivo);
-            yield return CambiarEscala(escalaObjetivo, escalaInicial);
+            // yield return CambiarEscala(escalaObjetivo, escalaInicial);
 
         }
     }
@@ -36,11 +36,35 @@ public class EscalarAlturaCubo : MonoBehaviour
         {
             tiempoTranscurrido += Time.deltaTime;
             float progreso = tiempoTranscurrido / duracion;
-            float escalaY = Mathf.Lerp(desde.y, hasta.y, progreso);
+
+            // interpolacion lineal para modificar la escala
+            // float escalaY = Mathf.Lerp(desde.y, hasta.y, progreso);
+
+            // interpolación con una pequeña aceleración
+            float escalaY = Mathf.SmoothStep(desde.y, hasta.y, progreso);
             transform.localScale = new Vector3(1, escalaY, 1);
 
             transform.position = new Vector3(transform.position.x, escalaY / 2, transform.position.z);
+            CambiarColor();
+
             yield return null;
+            escalaInicial = transform.localScale;
+        }
+    }
+
+    private void CambiarColor()
+    {
+        if (transform.localScale.y >= escalaMaxima * 0.9f)
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        else if (transform.localScale.y < escalaMinima + escalaMaxima * 0.1f)
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = Color.white;
         }
     }
 }
